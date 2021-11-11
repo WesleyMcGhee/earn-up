75fe92a66d3a0a54de78d80f53dfbf68c33069a8
Wesley McGhee <wesmcghee6372@gmail.com>
2021-11-11 12:48:09 -0600

    added some more back end for JWT

--- server/controller.js
+++ server/controller.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const pool = require("./db");
-
+const jwtGenerator = require("./utils/jwtGenerator");
 module.exports = {
   postSignup: async (req, res) => {
     try {
@@ -10,15 +10,18 @@ module.exports = {
         "SELECT * FROM users WHERE username = $1;",
         [username]
       );
-      if (userCheck.rows[0]) {
-        res.status(200).send("");
+      if (userCheck.rows.length !== 0) {
+        return res
+          .status(401)
+          .send("User already exsit! You have to be original UwU");
       }
       const hashedPassword = await bcrypt.hash(password, 10);
       const newUser = await pool.query(
-        "INSERT INTO users (username, password) VALUES ($1, $2);",
+        "INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *;",
         [username, hashedPassword]
       );
-      res.json(newUser.rows[0]);
+      const token = jwtGenerator(newUser.rows[0].id);
+      res.json({ token });
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Internal Server Error! R Nar >_<");
@@ -27,23 +30,22 @@ module.exports = {
   postLogin: async (req, res) => {
     try {
       const { username, password } = req.body;
-      const salt = bcrypt.genSaltSync(10);
-      const passhash = bcrypt.hashSync(password, salt);
       const userLog = await pool.query(
-        "SELECT id, username, password FROM users WHERE username = $1;",
+        "SELECT * FROM users WHERE username = $1;",
         [username]
       );
-      let actUser = userLog.rows[0];
-      if (actUser.username === username) {
-        let matched = bcrypt.compare(passhash, actUser.password);
-        if (matched) {
-          const accessToken = jwt.sign(username, process.env.TOKEN);
-          console.log("Works");
-          return res.status(200).send({ id: actUser.id, token: accessToken });
-        }
-      } else {
-        return res.status(200).send({ id: -1 });
+      if (userLog.rows.length === 0) {
+        return res.status(401).send("User Does Not Exist");
       }
+      const validPassword = await bcrypt.compare(
+        password,
+        userLog.rows[0].password
+      );
+      if (!validPassword) {
+        return res.status(401).send("Invalid Password Attempt");
+      }
+      const jwtToken = jwtGenerator(userLog.rows[0].id);
+      return res.json({ jwtToken });
     } catch (err) {
       console.error(err);
     }
